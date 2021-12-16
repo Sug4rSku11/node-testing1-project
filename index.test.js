02412569f316304a7b7d79e7e82566078538a21a
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-15 18:35:08 -0700

    completed exercise 7, passing

--- index.test.js
+++ index.test.js
@@ -98,17 +98,25 @@ describe('[Exercise 5] Seasons', () => {
 })
 
 describe('[Exercise 6] Car', () => {
-//  let focus
-//  beforeEach(() => {
-//    focus = new utils.Car('focus', 20, 30) // each test must start with a fresh car
- //// })
-  // test('[15] driving the car returns the updated odometer', () => {})
-  // test('[16] driving the car uses gas', () => {})
-  // test('[17] refueling allows to keep driving', () => {})
-  // test('[18] adding fuel to a full tank has no effect', () => {})
+ let focus
+ beforeEach(() => {
+   focus = new utils.Car('focus', 20, 30) // each test must start with a fresh car
+  })
+  test('[15] driving the car returns the updated odometer', () => {
+
+  })
+  test('[16] driving the car uses gas', () => {})
+  test('[17] refueling allows to keep driving', () => {})
+  test('[18] adding fuel to a full tank has no effect', () => {})
 })
 
 describe('[Exercise 7] isEvenNumberAsync', () => {
-  // test('[19] resolves true if passed an even number', () => {})
-  //test('[20] resolves false if passed an odd number', () => {})  
+   test('[19] resolves true if passed an even number', async () => {
+    const result = utils.isEvenNumberAsync(4)
+    expect(result).toBe(true)
+   })
+   test('[20] resolves false if passed an odd number', async () => {
+    const result = utils.isEvenNumberAsync(5)
+    expect(result).toBe(false)
+   })  
 })
